7f18887a980da0c90b965eeb77098a8d8142bfd2
Andy Piper <andypiper@users.noreply.github.com>
2020-06-26 14:07:51 +0100

    Update to fully hydrate Tweets by default.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using OAuth;
 using System.Net;
 using System.IO;
+using System.Text;
 
 namespace TwitterDotNetCore
 {
@@ -10,6 +11,10 @@ namespace TwitterDotNetCore
   {
     static void Main(string[] args)
     {
+
+      Console.WriteLine("Enter the Tweet ID: ");
+      string tweetID = Console.ReadLine(); // no error checking here!
+
       // convenient to load keys and tokens from a config file for testing
       // edit .env.sample (and rename to .env) to add your keys and tokens (no quotation marks)
       DotNetEnv.Env.Load();
@@ -20,7 +25,19 @@ namespace TwitterDotNetCore
       string ACCESS_TOKEN_SECRET = System.Environment.GetEnvironmentVariable("ACCESS_TOKEN_SECRET");
 
       // this is the endpoint we will be calling
-      string REQUEST_URL = "https://api.twitter.com/labs/2/tweets/20?tweet.fields=author_id,created_at,entities,source,public_metrics,lang,geo&expansions=author_id&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,public_metrics";
+      StringBuilder apiPath = new StringBuilder("https://api.twitter.com");
+      apiPath.Append("/labs/2/tweets");
+      apiPath.AppendFormat("?ids={0}", tweetID);
+      apiPath.Append("&tweet.fields=attachments,author_id,context_annotations,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,source,text,withheld");
+      apiPath.Append("&media.fields=duration_ms,height,media_key,preview_image_url,type,url,width");
+      apiPath.Append("&expansions=attachments.poll_ids,attachments.media_keys,author_id,geo.place_id,in_reply_to_user_id,referenced_tweets.id");
+      apiPath.Append("&poll.fields=duration_minutes,end_datetime,id,options,voting_status");
+      apiPath.Append("&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type");
+      apiPath.Append("&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld");
+      string REQUEST_URL = apiPath.ToString();
+
+      // if you would like to compare to v1.1 then this alternative REQUEST_URL does that for Tweet ID 20
+      // string REQUEST_URL = "https://api.twitter.com/labs/2/tweets/20?tweet.fields=author_id,created_at,entities,source,public_metrics,lang,geo&expansions=author_id&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,public_metrics";
 
       // Create a new connection to the OAuth server, with a helper method
       OAuthRequest client = OAuthRequest.ForProtectedResource("GET", CONSUMER_KEY, CONSUMER_TOKEN, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
@@ -31,7 +48,7 @@ namespace TwitterDotNetCore
       HttpWebRequest request = (HttpWebRequest)WebRequest.Create(client.RequestUrl);
       request.Headers.Add("Authorization", auth);
 
-      Console.WriteLine("Calling " + REQUEST_URL);
+      Console.WriteLine("\nCalling " + REQUEST_URL + "\n");
 
       // make the call and print the string value of the response JSON
       HttpWebResponse response = (HttpWebResponse)request.GetResponse();
--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 
 [![license](https://img.shields.io/badge/License-Apache%202.0-green.svg)](https://github.com/andypiper/TwitterDotNetCore/blob/master/LICENSE) [![Labs v2](https://img.shields.io/static/v1?label=Twitter%20API&message=Developer%20Labs%20v2&color=794BC4&style=flat&logo=Twitter)](https://developer.twitter.com/en/docs/labs/overview/versioning) [![Twitter Follow](https://badgen.net/twitter/follow/andypiper)](https://twitter.com/intent/follow?screen_name=andypiper)
 
-A simple sample app using DotNet Core and the Twitter API.
+A simple sample app using DotNet Core and the new Twitter API.
 
-By default, the code targets the [Twitter Developer Labs](https://t.co/labs) API, requesting @jack's first Tweet (Tweet ID 20), and the same fields that are returned in API v1.1. This can be changed by editing the value of `REQUEST_URL` in `Program.cs`.
+By default, the code prompts for a Tweet ID and then *fully hydrates* that via the [Twitter Developer Labs](https://t.co/labs) API. You may also enter a comma-separated list of IDs, as the API endpoint will also handle multiple IDs (up to 100).
+
+To have the code request @jack's first Tweet (Tweet ID 20), and the same fields that are returned in API v1.1, comment out the `REQUEST_URL` value in `Program.cs` and uncomment the replacement in the next section.
+
+### What does it mean to 'hydrate a Tweet'?
+Hydrating a Tweet means getting all available fields for a Tweet, given a Tweet ID (or a set of Tweet IDs)
 
 ## Directions
 
 [Apply for a Twitter Developer Account](https://t.co.apply-for-access), and create an app. Generate access token and access token secret via the Twitter Developer Dashboard.
 
-(optional) If using the [Twitter Developer Labs](https://t.co/labs) endpoint, you'll also need to have access to Labs, and to connect your app to the Tweets and Users endpoints.
+(optional) If using the [Twitter Developer Labs](https://t.co/labs) endpoint - as this sample does - you'll also need to have access to Labs, and to connect your app to the Tweets and Users endpoints.
 
 Install [DotNet Core](https://dotnet.microsoft.com/download) :-)
 
